a720342626e5209061d6f4be09efa03337417d9f
Erin Millard <ezzatron@gmail.com>
2013-03-15 12:39:52 +1000

    Data URIs working.

--- src/Eloquent/Schemer/Uri/DataUri.php
+++ src/Eloquent/Schemer/Uri/DataUri.php
@@ -11,9 +11,217 @@
 
 namespace Eloquent\Schemer\Uri;
 
+use Zend\Uri\Exception\InvalidUriException;
 use Zend\Uri\Uri;
 
 class DataUri extends Uri
 {
     protected static $validSchemes = array('data');
+
+    /**
+     * @param string|null $mimeType
+     */
+    public function setMimeType($mimeType)
+    {
+        $this->mimeType = $mimeType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMimeType()
+    {
+        if (empty($this->mimeType)) {
+            return 'text/plain;charset=US-ASCII';
+        }
+
+        return $this->mimeType;
+    }
+
+    /**
+     * @param string|null $encoding
+     */
+    public function setEncoding($encoding)
+    {
+        $isDifferent = $this->encoding !== $encoding;
+        if ($isDifferent) {
+            $data = $this->getData();
+        }
+
+        $this->encoding = $encoding;
+
+        if ($isDifferent) {
+            $this->setData($data);
+        }
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEncoding()
+    {
+        return $this->encoding;
+    }
+
+    /**
+     * @param string $rawData
+     */
+    public function setRawData($rawData)
+    {
+        $this->rawData = $rawData;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRawData()
+    {
+        return $this->rawData;
+    }
+
+    /**
+     * @param string $data
+     */
+    public function setData($data)
+    {
+        if ('base64' === $this->encoding) {
+            $this->setRawData(base64_encode($data));
+        } else {
+            $this->setRawData(rawurlencode($data));
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getData()
+    {
+        if ('base64' === $this->encoding) {
+            return base64_decode($this->getRawData());
+        }
+
+        return rawurldecode($this->getRawData());
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isValid()
+    {
+        if (
+            !empty($this->mimeType) &&
+            !preg_match(
+                '#^[\w-]+/[\w-]+(?:;\s*[\w-]+=[\w-]+)*$#',
+                $this->mimeType
+            )
+        ) {
+            return false;
+        }
+
+        if (!empty($this->encoding) && 'base64' !== $this->encoding) {
+            return false;
+        }
+
+        if (
+            'base64' === $this->encoding &&
+            !preg_match(
+                '#^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$#',
+                $this->rawData
+            )
+        ) {
+            return false;
+        } else if (!preg_match('/^[!#$&-;=?-[\]_a-z~%]+$/', $this->rawData)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $uri
+     *
+     * @return DataUri
+     */
+    public function parse($uri)
+    {
+        if (($scheme = static::parseScheme($uri)) !== null) {
+            $this->setScheme($scheme);
+            $uri = substr($uri, strlen($scheme) + 1);
+        }
+
+        $parts = explode(',', $uri, 2);
+
+        if (count($parts) > 1) {
+            $type = array_shift($parts);
+            if (';base64' === substr($type, -7)) {
+                $this->encoding = 'base64';
+                $type = substr($type, 0, -7);
+            }
+            $this->setMimeType($type);
+        }
+
+        $this->rawData = array_shift($parts);
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function toString()
+    {
+        if (!$this->isValid()) {
+            throw new InvalidUriException(
+                'URI is not valid and cannot be converted into a string'
+            );
+        }
+
+        $uri = '';
+        if ($this->scheme) {
+            $uri .= $this->scheme . ':';
+        }
+        if ($this->mimeType) {
+            $uri .= $this->mimeType;
+        }
+        if ($this->encoding) {
+            $uri .= ';' . $this->encoding;
+        }
+        $uri .= ',' . $this->rawData;
+
+        return $uri;
+    }
+
+    /**
+     * @return DataUri
+     */
+    public function normalize()
+    {
+        if ($this->scheme) {
+            $this->scheme = static::normalizeScheme($this->scheme);
+        }
+
+        if ($this->encoding) {
+            $this->encoding = static::normalizeEncoding($this->encoding);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $encoding
+     *
+     * @return string
+     */
+    protected static function normalizeEncoding($encoding)
+    {
+        if (preg_match('//', $encoding)) {
+            return null;
+        }
+
+        return $encoding;
+    }
+
+    protected $mimeType;
+    protected $encoding;
+    protected $rawData;
 }
