12e76fea8d9f07f61b098d93466b7dff4684b0dc
Erin Millard <ezzatron@gmail.com>
2013-04-15 07:42:56 +1000

    Implemented matchesByPointer for validation results.

--- src/Eloquent/Schemer/Validation/Result/ValidationResult.php
+++ src/Eloquent/Schemer/Validation/Result/ValidationResult.php
@@ -11,23 +11,34 @@
 
 namespace Eloquent\Schemer\Validation\Result;
 
+use Eloquent\Equality\Comparator;
+use Eloquent\Schemer\Pointer\PointerInterface;
+
 class ValidationResult
 {
     /**
      * @param array<ValidationIssue>|null $issues
      * @param array<ValidationMatch>|null $matches
+     * @param Comparator|null             $comparator
      */
-    public function __construct(array $issues = null, array $matches = null)
-    {
+    public function __construct(
+        array $issues = null,
+        array $matches = null,
+        Comparator $comparator = null
+    ) {
         if (null === $issues) {
             $issues = array();
         }
         if (null === $matches) {
             $matches = array();
         }
+        if ($comparator) {
+            $comparator = new Comparator;
+        }
 
         $this->issues = $issues;
         $this->matches = $matches;
+        $this->comparator = $comparator;
     }
 
     /**
@@ -39,6 +50,14 @@ class ValidationResult
     }
 
     /**
+     * @return boolean
+     */
+    public function isValid()
+    {
+        return count($this->issues) < 1;
+    }
+
+    /**
      * @return array<ValidationMatch>
      */
     public function matches()
@@ -47,11 +66,28 @@ class ValidationResult
     }
 
     /**
-     * @return boolean
+     * @return Comparator
      */
-    public function isValid()
+    public function comparator()
     {
-        return count($this->issues) < 1;
+        return $this->comparator;
+    }
+
+    /**
+     * @param PointerInterface $pointer
+     *
+     * @return array<ValidationMatch>
+     */
+    public function matchesByPointer(PointerInterface $pointer)
+    {
+        $matches = array();
+        foreach ($this->matches() as $match) {
+            if ($this->comparator->equals($match->pointer(), $pointer)) {
+                $matches[] = $match;
+            }
+        }
+
+        return $matches;
     }
 
     /**
@@ -69,4 +105,5 @@ class ValidationResult
 
     private $issues;
     private $matches;
+    private $comparator;
 }
